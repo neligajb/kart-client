d80f50a86aa3b55f223e55a6fa270ef80624b61b
Boone N <boone.neligan@snagajob.com>
2018-02-23 00:34:38 -0500

    remove unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -18,7 +16,6 @@ import { PlayerScorePipe } from './pipes/player-score.pipe'; // import our pipe 
 @NgModule({
   imports:      [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     AppRoutingModule
